c2d164975943d66deb8584d62dd8ec5b9822297b
GersonCalo <calo.gerson@uni.eus>
2023-12-31 04:05:26 +0100

    Spring Proyect create

---
+++ futbol-manager/src/test/java/dambi/futbolmanager/FutbolManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package dambi.futbolmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FutbolManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
