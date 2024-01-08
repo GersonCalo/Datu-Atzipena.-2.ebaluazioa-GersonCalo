997b1028312d1817406b1a5aaa211486404453cc
GersonCalo <calo.gerson@uni.eus>
2024-01-08 20:05:13 +0100

    correccion añadir datos de tipo int y double en la DB

--- datuakGeitu/src/main/java/damci/App.java
+++ datuakGeitu/src/main/java/damci/App.java
@@ -3,6 +3,9 @@ package damci;
 import java.io.FileReader;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
 import java.util.List;
 
 import org.bson.Document;
@@ -32,16 +35,44 @@ public class App {
 
                 for (int i = 1; i < records.size(); i++) {
                     String[] record = records.get(i);
+                    System.out.println("Procesando registro " + i + ": " + Arrays.toString(record));
                     Document document = new Document();
                     for (int j = 0; j < columnNames.length; j++) {
-                        document.append(columnNames[j], record[j]);
+                        String columnName = columnNames[j];
+                        String value = record[j];
+
+                        switch (columnName) {
+                            case "age":
+                            case "overall_rating":
+                            case "potential":
+                            case "international_reputation":
+                            case "weak_foot":
+                            case "skill_moves":
+                                int intValue = Integer.parseInt(value);
+                                document.append(columnName, intValue);
+                                break;
+                            case "height_cm":
+                            case "weight_kgs":
+                            case "value_euro":
+                            case "wage_euro":
+                                if (!value.isEmpty()) {
+                                    double doubleValue = Double.parseDouble(value);
+                                    document.append(columnName, doubleValue);
+                                } else {
+                                    document.append(columnName, 0.0);
+                                }
+
+                                break;
+                            default:
+                                document.append(columnName, value);
+                                break;
+                        }
                     }
                     collection.insertOne(document);
 
                     System.out.println("Documento insertado en MongoDB:");
                     System.out.println(document.toJson());
                     System.out.println("---------------------------------");
-
                 }
 
             } catch (Exception e) {
